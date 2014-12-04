ed0586bebb38f44490e07dbb09611f1a705e1e57
manu <roche.emmanuel@gmail.com>
2014-12-04 23:20:56 +0100

    Implemented nn_predict method and using it in gd_errfunc. Now need testing.

--- sources/nervCUDA/src/kernels/gd_errfunc.cu
+++ sources/nervCUDA/src/kernels/gd_errfunc.cu
@@ -5,47 +5,9 @@ template<typename T, unsigned int blockSize>
 void gd_errfunc_device(unsigned int nl, unsigned int np, unsigned int* lsizes, unsigned int nsamples,
 	T* d_params, T* d_X, T* d_yy, T lambda, T* J, T* d_grads, T* d_deltas, T* d_inputs, T* d_regw, T bias, cudaStream_t stream)
 {
-	// getLastCudaError("Checkpoint1");
-
 	unsigned int nt = nl-1; // number of matrices evolved.
 
-	// offset used to locate the theta_i matrix in the d_params array.
-	unsigned int theta_offset = 0;
-
-	// Offset used for the z(i) matrix on iteration i
-	int input_offset = 0;
-
-	int next_input_offset = 0; //nsamples*lsizes[1];
-
-  for(unsigned int i=0; i<nt;++i) {
-  	// We compute the activation and input values for the given layer:
-
-  	// The kernel compute the values of zi and a(i+1) 
-  	// (note that the value or a(0) is already loaded in the Activation vector).
-  	// even if we compute the a(i+1) matrix we actually discard completely the first column
-  	// in this matrix (colu of intercept terms). As a result we just need to mapped the GPU grid to
-  	// the dimension of of the sub z(i) matrix (which is transposed.)
-  	// THe dimensions for z(i) are: lsize(i+1) * nsamples
-  	// When this is transposed we get: nsamples * lsize(i+1);
-		unsigned int nrows = lsizes[i+1];
-		unsigned int ncolT = lsizes[i]; // we remove 1 here because we consider the intercept row as "virtual" in our calculation.
-		unsigned int ncols = nsamples;
-
-		dim3 dimBlock(blockSize, blockSize);
-		dim3 dimGrid((blockSize + ncols-1)/blockSize, (blockSize + nrows-1)/blockSize);
-
-		// Also we will need access to the theta_i matrix so we need to keep track of its global offset in the
-		// network parameters array.
-		// logDEBUG("Using grid size: ("<<dimGrid.x<<" x "<<dimGrid.y<<")");
-		ComputeActivation<<<dimGrid, dimBlock, 0, stream>>>(theta_offset, input_offset, next_input_offset,
-			nrows, ncols, ncolT, d_params, d_inputs, d_X, bias);
-		// CHECK_KERNEL();
-
-		// update the offsets:
-		theta_offset += lsizes[i+1]*(lsizes[i]+1);
-		input_offset = next_input_offset;
-		next_input_offset += nrows*ncols;
-  }
+  int input_offset = nn_activation_device(nl, lsizes, nsamples, d_params, d_X, d_inputs, bias, stream);
 
   T* d_hx = d_inputs + input_offset;
 
@@ -85,7 +47,8 @@ void gd_errfunc_device(unsigned int nl, unsigned int np, unsigned int* lsizes, u
 
 	// remove the last theta matrix size from the theta offset so that we can use
 	// that offset to retrieve the proper theta matrix:
-	theta_offset -= lsizes[nt]*(lsizes[nt-1]+1);
+	// theta_offset -= lsizes[nt]*(lsizes[nt-1]+1);
+	unsigned int theta_offset = np - lsizes[nt]*(lsizes[nt-1]+1);
 
 	// initially the input_offset is pointing on the hx matrix which is z(nt-1) with our convention (eg. z(0) is not in the array.)
 	// But the first one we will need is actually the one before that: z(nt-2)
---
+++ sources/nervCUDA/src/kernels/nn_activation.cu
@@ -0,0 +1,112 @@
+#include <nervCUDA.h>
+#include <nerv_kernels.h>
+
+template<typename T, unsigned int blockSize>
+int nn_activation_device(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, 
+	T* d_params, T* d_X, T* d_inputs, T bias, cudaStream_t stream)
+{
+	unsigned int nt = nl-1; // number of matrices evolved.
+
+	// offset used to locate the theta_i matrix in the d_params array.
+	unsigned int theta_offset = 0;
+
+	// Offset used for the z(i) matrix on iteration i
+	int input_offset = 0;
+
+	int next_input_offset = 0; //nsamples*lsizes[1];
+
+  for(unsigned int i=0; i<nt;++i) {
+  	// We compute the activation and input values for the given layer:
+
+  	// The kernel compute the values of zi and a(i+1) 
+  	// (note that the value or a(0) is already loaded in the Activation vector).
+  	// even if we compute the a(i+1) matrix we actually discard completely the first column
+  	// in this matrix (colu of intercept terms). As a result we just need to mapped the GPU grid to
+  	// the dimension of of the sub z(i) matrix (which is transposed.)
+  	// THe dimensions for z(i) are: lsize(i+1) * nsamples
+  	// When this is transposed we get: nsamples * lsize(i+1);
+		unsigned int nrows = lsizes[i+1];
+		unsigned int ncolT = lsizes[i]; // we remove 1 here because we consider the intercept row as "virtual" in our calculation.
+		unsigned int ncols = nsamples;
+
+		dim3 dimBlock(blockSize, blockSize);
+		dim3 dimGrid((blockSize + ncols-1)/blockSize, (blockSize + nrows-1)/blockSize);
+
+		// Also we will need access to the theta_i matrix so we need to keep track of its global offset in the
+		// network parameters array.
+		// logDEBUG("Using grid size: ("<<dimGrid.x<<" x "<<dimGrid.y<<")");
+		ComputeActivation<<<dimGrid, dimBlock, 0, stream>>>(theta_offset, input_offset, next_input_offset,
+			nrows, ncols, ncolT, d_params, d_inputs, d_X, bias);
+		// CHECK_KERNEL();
+
+		// update the offsets:
+		theta_offset += lsizes[i+1]*(lsizes[i]+1);
+		input_offset = next_input_offset;
+		next_input_offset += nrows*ncols;
+  }
+
+  return input_offset;
+}
+
+
+// Explicit instanciation:
+template int nn_activation_device(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, 
+	double* d_params, double* d_X, double* d_inputs, double bias, cudaStream_t stream);
+
+template int nn_activation_device(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, 
+	float* d_params, float* d_X, float* d_inputs, float bias, cudaStream_t stream);
+
+
+template <typename T>
+void _nn_predict(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, 
+	T* params, T* X, T* hx, T bias)
+{
+	size_t size;
+	unsigned int nt = nl -1;
+
+	// Compute the number of parameters:
+	// and compute the number of activation (eg. inputs) values:
+	unsigned int np = 0;
+	unsigned int ni = 0;
+	for(unsigned int i=0;i<nt;++i) {
+		np += lsizes[i + 1] * (lsizes[i] + 1);
+    ni += lsizes[i + 1] * nsamples;
+	}
+
+	size = np * sizeof(T);
+	T* d_params = NULL;
+	checkCudaErrors(cudaMalloc(&d_params, size));
+	checkCudaErrors(cudaMemcpy(d_params, params, size, cudaMemcpyHostToDevice));
+	
+	size = lsizes[0]*nsamples * sizeof(T);
+	T* d_X = NULL;
+	checkCudaErrors(cudaMalloc(&d_X, size));
+	checkCudaErrors(cudaMemcpy(d_X, X, size, cudaMemcpyHostToDevice));
+
+	size = ni * sizeof(T);
+	T* d_inputs = NULL;
+	checkCudaErrors(cudaMalloc(&d_inputs, size));
+
+ 	int input_offset = nn_activation_device(nl,lsizes,nsamples,d_params,d_X,d_inputs,bias);
+
+ 	size = lsizes[nt]*nsamples * sizeof(T);
+	checkCudaErrors(cudaMemcpy(hx, d_inputs+input_offset, size, cudaMemcpyDeviceToHost));
+
+	checkCudaErrors(cudaFree(d_params));
+	checkCudaErrors(cudaFree(d_X));
+	checkCudaErrors(cudaFree(d_inputs));
+}
+
+extern "C" {
+
+void nn_predict(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, double* params, double* X, double* hx, double bias)
+{
+	_nn_predict(nl, lsizes, nsamples, params, X, hx, bias);
+}
+
+void nn_predict_f(unsigned int nl, unsigned int* lsizes, unsigned int nsamples, float* params, float* X, float* hx, float bias)
+{
+	_nn_predict(nl, lsizes, nsamples, params, X, hx, bias);
+}
+
+}
