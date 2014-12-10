f30fd83d405635c8d3a2192c020216fe18ec99fd
manu <roche.emmanuel@gmail.com>
2014-12-10 23:09:36 +0100

    Refactoring some kernels.

--- sources/nervCUDA/src/kernels/compute_activation.cu
+++ sources/nervCUDA/src/kernels/compute_activation.cu
@@ -5,74 +5,90 @@ template <typename T, unsigned int blockSize>
 __global__ void ComputeActivation(BPComputeTraits<T> traits)
 {
 
-	unsigned int nrows = traits.nrows;
-	unsigned int ncols = traits.ncols;
-	unsigned int ncolT = traits.niter;
+  unsigned int nrows = traits.nrows;
+  unsigned int ncols = traits.ncols;
+  unsigned int ncolT = traits.niter;
 
-	// Note that we assume here that the matrix coefficient are stored in row major order:
-	// eg Aelem(i,jl) = A[j*nrowA+i]
-  int row = blockIdx.y*blockSize + threadIdx.x;
-  int col = blockIdx.x*blockSize + threadIdx.y;
+  // Note that we assume here that the matrix coefficient are stored in row major order:
+  // eg Aelem(i,jl) = A[j*nrowA+i]
+  int row = blockIdx.y * blockSize + threadIdx.x;
+  int col = blockIdx.x * blockSize + threadIdx.y;
 
-  __shared__ T As[blockSize][blockSize+1];
-  __shared__ T Bs[blockSize][blockSize+1];
+  __shared__ T As[blockSize][blockSize + 1];
+  __shared__ T Bs[blockSize][blockSize + 1];
 
   int xx, yy;
 
   // we can already add the element on the first row of theta_i to this element value:
   // but note that this element should be multiplied with the desired bias:
-  T zval = traits.params[traits.theta_offset + row]* traits.bias;
+  T zval = traits.params[traits.theta_offset + row] * traits.bias;
+  T val;
 
   // Here we compute the product theta_i * a_i^T
-  for (int k = 0; k < (blockSize + ncolT - 1)/blockSize; k++) {
+  for (int k = 0; k < (blockSize + ncolT - 1) / blockSize; k++)
+  {
 
-  	xx = k*blockSize + threadIdx.y;
-  	yy = blockIdx.y*blockSize + threadIdx.x;
+    xx = k * blockSize + threadIdx.y;
+    yy = blockIdx.y * blockSize + threadIdx.x;
 
-		if (xx < ncolT && yy < nrows) 
-			// Note here that we should NOT use the first row of theta_i in those computation:
-			// That row elemtn is already added to the zval value (matching the "virtual" 1 row
-			// on top of the z_i matrix when used as activation.)
-		 	As[threadIdx.x][threadIdx.y] = traits.params[traits.theta_offset + (xx+1)*nrows + yy];
-		else
-			As[threadIdx.x][threadIdx.y] = 0.0;
+    val = 0.0;
 
+    if (xx < ncolT && yy < nrows)
+    {
+      // Note here that we should NOT use the first row of theta_i in those computation:
+      // That row elemtn is already added to the zval value (matching the "virtual" 1 row
+      // on top of the z_i matrix when used as activation.)
+      val = traits.params[traits.theta_offset + (xx + 1) * nrows + yy];
+    }
 
-		if(traits.next_input_offset==0) {
-			// In that case we need to retrieve the data from the X matrix.
-			// actually we need the data from X^T.
-			xx = blockIdx.x*blockSize + threadIdx.x;
-			yy = k*blockSize + threadIdx.y;
+    As[threadIdx.x][threadIdx.y] = val;
 
-			if (xx < ncols && yy < ncolT)
-				Bs[threadIdx.y][threadIdx.x] = traits.X[yy*ncols + xx];
-			else
-				Bs[threadIdx.y][threadIdx.x] = 0.0;
-		}
-		else {
-			xx = blockIdx.x*blockSize + threadIdx.y;
-			yy = k*blockSize + threadIdx.x;
+    val = 0.0;
 
-			if (yy < ncolT && xx < ncols)
-				Bs[threadIdx.x][threadIdx.y] = traits.inputs[traits.input_offset + xx*ncolT + yy];
-			else
-				Bs[threadIdx.x][threadIdx.y] = 0.0;
-		}
+    if (traits.next_input_offset == 0)
+    {
+      // In that case we need to retrieve the data from the X matrix.
+      // actually we need the data from X^T.
+      xx = blockIdx.x * blockSize + threadIdx.x;
+      yy = k * blockSize + threadIdx.y;
 
-		__syncthreads();
+      if (xx < ncols && yy < ncolT)
+      {
+        val = traits.X[yy * ncols + xx];
+      }
 
-		for (int n = 0; n < blockSize; ++n) 
-			zval += As[threadIdx.x][n] * Bs[n][threadIdx.y];
+      Bs[threadIdx.y][threadIdx.x] = val;
+    }
+    else
+    {
+      xx = blockIdx.x * blockSize + threadIdx.y;
+      yy = k * blockSize + threadIdx.x;
 
-		__syncthreads();
+      if (yy < ncolT && xx < ncols)
+      {
+        val = traits.inputs[traits.input_offset + xx * ncolT + yy];
+      }
+
+      Bs[threadIdx.x][threadIdx.y] = val;
+    }
+
+    __syncthreads();
+
+    for (int n = 0; n < blockSize; ++n)
+    {
+      zval += As[threadIdx.x][n] * Bs[n][threadIdx.y];
+    }
+
+    __syncthreads();
   }
 
-  if (row < nrows && col < ncols) {
-  	// compute the sigmoid of the value:
-  	zval = 1.0 / (1.0 + exp(-zval* traits.wmult));
+  if (row < nrows && col < ncols)
+  {
+    // compute the sigmoid of the value:
+    zval = 1.0 / (1.0 + exp(-zval * traits.wmult));
 
-  	// we just computed the value z_i(row,col), now we store it:
-  	traits.inputs[traits.next_input_offset + nrows*col + row] = zval;
+    // we just computed the value z_i(row,col), now we store it:
+    traits.inputs[traits.next_input_offset + nrows * col + row] = zval;
   }
 
 }
--- sources/nervCUDA/src/kernels/compute_delta.cu
+++ sources/nervCUDA/src/kernels/compute_delta.cu
@@ -3,59 +3,69 @@
 
 template<typename T, unsigned int blockSize>
 __global__ void ComputeDelta(BPComputeTraits<T> traits)
-	// unsigned int theta_offset, unsigned int input_offset,  unsigned int delta_offset, unsigned int next_delta_offset,
-	// unsigned int nrows, unsigned int ncols, unsigned int niter, T* nn_params, T* inputs, T* deltas) 
+// unsigned int theta_offset, unsigned int input_offset,  unsigned int delta_offset, unsigned int next_delta_offset,
+// unsigned int nrows, unsigned int ncols, unsigned int niter, T* nn_params, T* inputs, T* deltas)
 {
-	// This operation is basically a matrix multiplication with transposition on A:
+  // This operation is basically a matrix multiplication with transposition on A:
   T dval = 0.0;
+  T val;
 
   unsigned int nrows = traits.nrows;
   unsigned int ncols = traits.ncols;
   unsigned int niter = traits.niter;
 
-  __shared__ T As[blockSize][blockSize+1];
-  __shared__ T Bs[blockSize][blockSize+1];
+  __shared__ T As[blockSize][blockSize + 1];
+  __shared__ T Bs[blockSize][blockSize + 1];
 
   // So we want to compute the value d(row,col);
   // note that since we transpose A, the A number of cols is nrows and its number of row is ncols.
   int xx, yy;
-  for (int k = 0; k < (blockSize + ncols - 1)/blockSize; k++) {
+  for (int k = 0; k < (blockSize + ncols - 1) / blockSize; k++)
+  {
 
-  	xx = k*blockSize + threadIdx.x;
-  	yy = blockIdx.y*blockSize + threadIdx.y;
-  	
-		if (yy < nrows && xx < niter) {
-			// We add 1 below because we do not want to use the first row of theta_T, so that's
-			// actually the first col of theta.
-			As[threadIdx.y][threadIdx.x] = traits.params[traits.theta_offset + (yy+1)*niter + xx]; 
-		}
-		else
-			As[threadIdx.y][threadIdx.x] = 0.0;
+    xx = k * blockSize + threadIdx.x;
+    yy = blockIdx.y * blockSize + threadIdx.y;
 
-		xx = blockIdx.x*blockSize + threadIdx.y;
-		yy = k*blockSize + threadIdx.x;
+    val = 0.0;
+    if (yy < nrows && xx < niter)
+    {
+      // We add 1 below because we do not want to use the first row of theta_T, so that's
+      // actually the first col of theta.
+      val = traits.params[traits.theta_offset + (yy + 1) * niter + xx];
+    }
 
-		if (yy < niter && xx < ncols)
-			Bs[threadIdx.x][threadIdx.y] = traits.deltas[traits.delta_offset + xx*niter + yy];
-		else
-			Bs[threadIdx.x][threadIdx.y] = 0.0;
+    As[threadIdx.y][threadIdx.x] = val;
 
-		__syncthreads();
+    xx = blockIdx.x * blockSize + threadIdx.y;
+    yy = k * blockSize + threadIdx.x;
 
-		for (int n = 0; n < blockSize; ++n) 
-			dval += As[threadIdx.x][n] * Bs[n][threadIdx.y];
+    val = 0.0;
+    if (yy < niter && xx < ncols)
+    {
+      val = traits.deltas[traits.delta_offset + xx * niter + yy];
+    }
 
-		__syncthreads();
+    Bs[threadIdx.x][threadIdx.y] = val;
+
+    __syncthreads();
+
+    for (int n = 0; n < blockSize; ++n)
+    {
+      dval += As[threadIdx.x][n] * Bs[n][threadIdx.y];
+    }
+
+    __syncthreads();
   }
 
-  int row = blockIdx.y*blockSize + threadIdx.x;
-  int col = blockIdx.x*blockSize + threadIdx.y;
+  int row = blockIdx.y * blockSize + threadIdx.x;
+  int col = blockIdx.x * blockSize + threadIdx.y;
 
-  if (row < nrows && col < ncols) {
-  	// we have to multiply that value by the corresponding sigmoid gradient value from the input matrix at the same location.
-  	int index = nrows*col+row;
-  	T sig = traits.inputs[traits.input_offset + index];
-  	traits.deltas[traits.next_delta_offset + index] = dval *sig*(1.0 - sig);
+  if (row < nrows && col < ncols)
+  {
+    // we have to multiply that value by the corresponding sigmoid gradient value from the input matrix at the same location.
+    int index = nrows * col + row;
+    T sig = traits.inputs[traits.input_offset + index];
+    traits.deltas[traits.next_delta_offset + index] = dval * sig * (1.0 - sig);
   }
 }
 
