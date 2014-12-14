0ea55cfa8cb4c7a22f09c0ca3c93ccdc4c2159e7
manu <roche.emmanuel@gmail.com>
2014-12-14 14:02:16 +0100

    Added test for threshold 0.0 and 1.0 on values rand_weights.

--- sources/test_nervcuda/src/rand_weights_spec.cpp
+++ sources/test_nervcuda/src/rand_weights_spec.cpp
@@ -208,4 +208,70 @@ BOOST_AUTO_TEST_CASE( test_rand_weights_debug )
   BOOST_CHECK(FreeLibrary(h));
 }
 
+BOOST_AUTO_TEST_CASE( test_rand_weights_values )
+{
+  typedef double value_type;
+  value_type epsilon = std::numeric_limits<value_type>::epsilon();
+
+  HMODULE h = LoadLibrary("nervCUDA.dll");
+  BOOST_CHECK(h != nullptr);
+
+  typedef void (*Func)(RandTraits<value_type> &traits);
+
+  // We should be able to retrieve the train function:
+  Func rand_weights = (Func) GetProcAddress(h, "rand_weights");
+  BOOST_CHECK(rand_weights != nullptr);
+
+  unsigned int num = 10; // number of tests to perform.
+
+  for (unsigned int i = 0; i < num; ++i)
+  {
+    unsigned int n = random_uint(1000, 1500);
+
+    value_type *weights = new value_type[n];
+    value_type val = random_real((value_type)0.0, (value_type)1.0);
+
+    // prepare the input values:
+    value_type *values = new value_type[n];
+    for (unsigned int j = 0; j < n; ++j)
+    {
+      values[j] = random_real((value_type)0.0, (value_type)1.0);
+    }
+
+    RandTraits<value_type> traits;
+    traits.target = weights;
+    traits.threshold = 1.0;
+    traits.size = n;
+    traits.values = values;
+
+    // generate the weights:
+    rand_weights(traits);
+
+    // Now compate the hx arrays:
+    for (unsigned int j = 0; j < n; ++j)
+    {
+      value_type v1 = weights[j];
+      value_type v2 = values[j];
+      BOOST_CHECK_MESSAGE(abs(v1 - v2) <= epsilon, "Mismatch (thres==1.0) on weight element " << j << ": " << v1 << "!=" << v2);
+    }
+
+    traits.threshold = 0.0;
+
+    // Now do the generation with threshold of 0:
+    // generate the weights:
+    rand_weights(traits);
+
+    // Now compate the hx arrays:
+    for (unsigned int j = 0; j < n; ++j)
+    {
+      value_type v1 = weights[j];
+      BOOST_CHECK_MESSAGE(abs(v1) <= epsilon, "Mismatch (thres==0.0) on weight element " << j << ": " << v1 << "!= 0.0");
+    }
+
+    delete [] weights;
+  }
+
+  BOOST_CHECK(FreeLibrary(h));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
