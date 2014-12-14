12740c7cc344c187ed9c86c0090fafee42f3964f
manu <roche.emmanuel@gmail.com>
2014-12-14 14:28:16 +0100

    Added test for debug rad weights with input values buffer.

--- sources/test_nervcuda/src/rand_weights_spec.cpp
+++ sources/test_nervcuda/src/rand_weights_spec.cpp
@@ -344,4 +344,77 @@ BOOST_AUTO_TEST_CASE( test_rand_weights_ratio_values )
   BOOST_CHECK(FreeLibrary(h));
 }
 
+BOOST_AUTO_TEST_CASE( test_rand_weights_debug_values )
+{
+  typedef double value_type;
+  value_type epsilon = std::numeric_limits<value_type>::epsilon();
+
+  HMODULE h = LoadLibrary("nervCUDA.dll");
+  BOOST_CHECK(h != nullptr);
+
+  typedef void (*Func)(RandTraits<value_type> &traits);
+
+  // We should be able to retrieve the train function:
+  Func rand_weights = (Func) GetProcAddress(h, "rand_weights");
+  BOOST_CHECK(rand_weights != nullptr);
+
+  unsigned int num = 10; // number of tests to perform.
+
+  for (unsigned int i = 0; i < num; ++i)
+  {
+    unsigned int n = random_uint(1000, 1500);
+
+    value_type *weights = new value_type[n];
+    value_type threshold = random_real((value_type)0.1, (value_type)0.9);
+
+    // prepare the input values:
+    value_type *values = new value_type[n];
+    for (unsigned int j = 0; j < n; ++j)
+    {
+      values[j] = random_real((value_type)0.0, (value_type)1.0);
+    }
+
+    RandTraits<value_type> traits;
+    traits.target = weights;
+    traits.threshold = threshold;
+    traits.size = n;
+    traits.values = values;
+
+    // enable debug mode:
+    traits.debug = true;
+
+    // generate the weights:
+    rand_weights(traits);
+
+    value_type zeros = 0.0;
+    value_type nzeros = 0.0;
+
+    // Now compate the hx arrays:
+    for (unsigned int j = 0; j < n; ++j)
+    {
+      if (weights[j] == 0.0)
+      {
+        zeros += 1.0;
+      }
+      else
+      {
+        nzeros += 1.0;
+      };
+
+      value_type v1 = weights[j];
+      value_type v2 = (abs(sin(j)) <= threshold) ? values[j] : 0.0;
+
+      BOOST_CHECK_MESSAGE(abs(v1 - v2) <= epsilon, "Mismatch on weight element " << j << ": " << v1 << "!=" << v2);
+    }
+
+    BOOST_CHECK(zeros > 0.0);
+    BOOST_CHECK(nzeros > 0.0);
+
+    delete [] weights;
+    delete [] values;
+  }
+
+  BOOST_CHECK(FreeLibrary(h));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
