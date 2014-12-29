9966e14f06796a195375aeac788f4ef57ac0d5b7
manu <roche.emmanuel@gmail.com>
2014-12-29 09:55:48 +0100

    Added norm test for softmax.

--- sources/test_nervcuda/src/predict_spec.cpp
+++ sources/test_nervcuda/src/predict_spec.cpp
@@ -323,4 +323,93 @@ BOOST_AUTO_TEST_CASE( test_nn_predict_with_softmax )
   BOOST_CHECK(FreeLibrary(h));
 }
 
+BOOST_AUTO_TEST_CASE( test_nn_predict_with_dropout_and_softmax )
+{
+  typedef double value_type;
+  value_type epsilon = std::numeric_limits<value_type>::epsilon();
+
+  HMODULE h = LoadLibrary("nervCUDA.dll");
+  BOOST_CHECK(h != nullptr);
+
+  typedef void (*PredictFunc)(BPTraits<value_type> &traits);
+
+  // We should be able to retrieve the train function:
+  PredictFunc nn_predict = (PredictFunc) GetProcAddress(h, "nn_predict");
+  BOOST_CHECK(nn_predict != nullptr);
+
+  PredictFunc nn_predict_cpu = (PredictFunc) GetProcAddress(h, "nn_predict_cpu");
+  BOOST_CHECK(nn_predict_cpu != nullptr);
+
+  unsigned int num = 10; // number of tests to perform.
+
+  for (unsigned int i = 0; i < num; ++i)
+  {
+    // Prepare a random training set:
+    TrainingSet<value_type> tr(std::vector<unsigned int> {100, 100, 100, 32}, 1000, TrainingSet<value_type>::TRAIN_DEBUG);
+    // TrainingSet<value_type> tr(std::vector<unsigned int>{50,30,20,10},1000,TrainingSet<value_type>::TRAIN_DEBUG);
+    // TrainingSet<value_type> tr(std::vector<unsigned int>{5,4,3},10,TrainingSet<value_type>::TRAIN_DEBUG);
+
+    value_type bias = tr.random_real(0.0, 1.0);
+
+    // Prepare the matrices for hx and pred_hx:
+    unsigned int ny = tr.y_train_size();
+    value_type *hx = tr.createArray(ny);
+    value_type *pred_hx = tr.createArray(ny);
+
+    // Prepare the weight multipliers:
+    value_type *dropouts = tr.createArray(tr.nt());
+    for (unsigned int j = 0; j < tr.nt(); ++j)
+    {
+      dropouts[j] = tr.random_real(0.0, 1.0);
+    }
+
+    // dropouts[0] = 1.0;
+    // dropouts[1] = 0.9;
+
+    // Now compute the predictions:
+    BPTraits<double> traits;
+    traits.nl = tr.nl();
+    traits.lsizes = tr.lsizes();
+    traits.nsamples_train = tr.nsamples();
+    traits.params = tr.params();
+    traits.X = tr.X_train();
+    traits.inputs = tr.createArray(traits.nd());
+    traits.bias = bias;
+    traits.dropouts = dropouts;
+    traits.hx = hx;
+    traits.use_softmax = true;
+
+    // Ensure that we perform prediction with debug mode activated:
+    traits.debug = true;
+
+    nn_predict(traits);
+
+    // Now compute the cpu version:
+    traits.hx = pred_hx;
+    nn_predict_cpu(traits);
+
+    // Now compare the hx arrays:
+    for (unsigned int j = 0; j < ny; ++j)
+    {
+      value_type v1 = hx[j];
+      value_type v2 = pred_hx[j];
+      BOOST_CHECK_MESSAGE(abs(v1 - v2) <= 100 * epsilon, "Mismatch (with dropout and softmax) on hx element " << j << ": " << v1 << "!=" << v2);
+    }
+
+    // Check that for each column in hx the sum of the predictions is close to 1.0:
+    unsigned int nout = traits.lsizes[traits.nl-1];
+    value_type tval;
+    for (unsigned int j = 1; j < traits.nsamples_train; ++j)
+    {
+      tval = 0.0;
+      for(unsigned int r=0;r<nout;++r) {
+        tval += hx[nout*j+r];
+      }
+      BOOST_CHECK_MESSAGE(abs(tval - 1.0) <= 10 * epsilon, "Invalid probability sum for sample " << j << ": " << tval << "!=" << 1.0);
+    }    
+  }
+
+  BOOST_CHECK(FreeLibrary(h));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
