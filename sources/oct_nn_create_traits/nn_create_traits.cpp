8ef3c88d10829012aaaba7e9fe83877afe1c9308
manu <roche.emmanuel@gmail.com>
2015-01-08 13:29:51 +0100

    Built nn_create_traits plugin.

--- sources/oct_nn_create_traits/nn_create_traits.cpp
+++ sources/oct_nn_create_traits/nn_create_traits.cpp
@@ -50,19 +50,19 @@ DEFUN_DLD (nn_create_traits, args, nargout,
   CHECK_RET(lsizes_val.is_matrix_type(), "nn_create_traits: lsizes is not a matrix type");
 
   // Assign the nl value:
-  Matrix lsizes_mat = lsizes_val.matrix_value();
-  unsigned int nl = lsizes_mat.numel();
+  Matrix lsizes = lsizes_val.matrix_value();
+  unsigned int nl = lsizes.numel();
   unsigned int nt = nl - 1;
   traits.nl = nl;
   // logDEBUG("traits.nl = " << traits.nl)
 
   // Assign the lsizes value:
-  unsigned int *lsizes = new unsigned int[nl];
+  unsigned int *lsizes_arr = new unsigned int[nl];
   for (unsigned int i = 0; i < nl; ++i)
   {
-    lsizes[i] = lsizes_mat(i);
+    lsizes_arr[i] = lsizes(i);
   }
-  traits.lsizes = lsizes;
+  traits.lsizes = lsizes_arr;
 
 
 
@@ -126,51 +126,51 @@ DEFUN_DLD (nn_create_traits, args, nargout,
   octave_value val = desc.contents("spaeBeta");
   if (val.is_defined())
   {
-    CHECK(val.is_double_type(), "nn_create_traits: spaeBeta is not a double type");
-    CHECK(0.0 <= val.double_value(), "nn_create_traits: out of range spaeBeta value");
+    CHECK_RET(val.is_double_type(), "nn_create_traits: spaeBeta is not a double type");
+    CHECK_RET(0.0 <= val.double_value(), "nn_create_traits: out of range spaeBeta value");
     traits.spae_beta = val.double_value();
   }
 
   val = desc.contents("spaeSparsity");
   if (val.is_defined())
   {
-    CHECK(val.is_double_type(), "nn_create_traits: spaeSparsity is not a double type");
-    CHECK(0.0 < val.double_value() && val.double_value() < 1.0, "nn_create_traits: out of range spaeSparsity value");
+    CHECK_RET(val.is_double_type(), "nn_create_traits: spaeSparsity is not a double type");
+    CHECK_RET(0.0 < val.double_value() && val.double_value() < 1.0, "nn_create_traits: out of range spaeSparsity value");
     traits.spae_sparsity = val.double_value();
   }
 
   val = desc.contents("useSoftmax");
   if (val.is_defined())
   {
-    CHECK(val.is_bool_type(), "nn_create_traits: useSoftmax is not a bool type");
+    CHECK_RET(val.is_bool_type(), "nn_create_traits: useSoftmax is not a bool type");
     traits.use_softmax = val.bool_value();
   }
 
   val = desc.contents("costMode");
   if (val.is_defined())
   {
-    CHECK(val.is_double_type(), "nn_create_traits: costMode is not a double type");
+    CHECK_RET(val.is_double_type(), "nn_create_traits: costMode is not a double type");
     traits.cost_mode = (unsigned int)val.double_value();
   }
 
   val = desc.contents("debug");
   if (val.is_defined())
   {
-    CHECK(val.is_bool_type(), "nn_create_traits: debug is not a bool type");
+    CHECK_RET(val.is_bool_type(), "nn_create_traits: debug is not a bool type");
     traits.debug = val.bool_value();
   }
 
   val = desc.contents("bias");
   if (val.is_defined())
   {
-    CHECK(val.is_double_type(), "nn_create_traits: bias is not a double type");
+    CHECK_RET(val.is_double_type(), "nn_create_traits: bias is not a double type");
     traits.bias = val.double_value();
   }
 
   val = desc.contents("lambda");
   if (val.is_defined())
   {
-    CHECK(val.is_double_type(), "nn_create_traits: lambda is not a double type");
+    CHECK_RET(val.is_double_type(), "nn_create_traits: lambda is not a double type");
     traits.lambda = val.double_value();
   }
 
@@ -178,15 +178,15 @@ DEFUN_DLD (nn_create_traits, args, nargout,
   val = desc.contents("dropouts");
   if (val.is_defined())
   {
-    CHECK(val.is_matrix_type(), "nn_create_traits: dropouts is not a matrix type");
+    CHECK_RET(val.is_matrix_type(), "nn_create_traits: dropouts is not a matrix type");
     Matrix drop_mat = val.matrix_value();
-    CHECK(drop_mat.numel() == nt, "nn_create_traits: invalid size for dropout matrix size: " << drop_mat.numel() << "!=" << nt);
+    CHECK_RET(drop_mat.numel() == nt, "nn_create_traits: invalid size for dropout matrix size: " << drop_mat.numel() << "!=" << nt);
 
     dropouts = new double[nt];
     for (unsigned int i = 0; i < nt; ++i)
     {
       dropouts[i] = drop_mat(i);
-      CHECK(dropouts[i] >= 0.0 && dropouts[i] <= 1.0, "nn_create_traits: dropout for layer " << i << " is out of range");
+      CHECK_RET(dropouts[i] >= 0.0 && dropouts[i] <= 1.0, "nn_create_traits: dropout for layer " << i << " is out of range");
     }
 
     traits.dropouts = dropouts;
@@ -204,10 +204,10 @@ DEFUN_DLD (nn_create_traits, args, nargout,
   int id = g_intf.create_device_traits(traits);
 
   // release the resources:
-  delete [] lsizes;
+  delete [] lsizes_arr;
   delete [] dropouts;
   
-  CHECK(id>0, "ERROR: exception occured in nn_create_traits.")
+  CHECK_RET(id>0, "ERROR: exception occured in nn_create_traits.")
 
   result.append(octave_uint32(id));
 
